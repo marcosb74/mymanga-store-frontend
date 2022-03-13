054d861f102e673d08b0855e50420a9f20541f74
as-takeba <markos.de.vl@gmail.com>
2022-03-13 16:54:54 -0300

    test

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default async function fetchProducts() {
+  console.log(process.env.REACT_APP_BASE_URL);
   return await axios.get(
     process.env.REACT_APP_BASE_URL + "api/listing/products"
   );
